245c2f68fcb1d3df935c1edefe69112d18bea31c
Exulten <stem.danieljohnbucad@gmail.com>
2025-05-09 00:32:50 +0800

    Retrieve Logs

--- app/src/main/java/com/example/gatekeep/MainActivity.java
+++ app/src/main/java/com/example/gatekeep/MainActivity.java
@@ -6,7 +6,6 @@ import android.content.IntentFilter;
 import android.nfc.NfcAdapter;
 import android.nfc.Tag;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.TableLayout;
 import android.widget.TableRow;
